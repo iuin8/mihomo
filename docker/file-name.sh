5cea27d1f4b01e942d3c9cbfddeb298b33a4bf76
刘金发 <liujinfa9242@dingtalk.com>
2026-03-28 03:48:17 +0800

    ci: 移除不再支持的平台构建
    移除对 linux/386 和 linux/arm/v7 平台的支持，并将 amd64 架构升级至 v3 版本

--- docker/file-name.sh
+++ docker/file-name.sh
@@ -2,16 +2,10 @@
 os="mihomo-linux-"
 case $TARGETPLATFORM in
     "linux/amd64")
-        arch="amd64-v1"
-        ;;
-    "linux/386")
-        arch="386"
+        arch="amd64-v3"
         ;;
     "linux/arm64")
         arch="arm64"
-        ;;
-    "linux/arm/v7")
-        arch="armv7"
         ;;
     "linux/riscv64")
         arch="riscv64"
